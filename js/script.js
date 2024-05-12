82e04b8512288ab5add99d148345bca30d79e10b
ceijoan <166540233+ceijoan@users.noreply.github.com>
2024-05-13 00:10:19 +0200

    actualizacion de estilos, a los enviados por pau y actualizacion de funcionalizad Script en pagina de ubicacion

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const chk_radio_a =document.querySelector('#acepto');
 const chk_radio_r =document.querySelector('#rechazo');
+const acepto =document.querySelector(".formulario__oculto");
 chk_radio_a.addEventListener("click",() => {
-    alert("HOLAAAAA");
+    //alert("HOLAAAAA");
+    acepto.classList.add("formulario__oculto--visible");
+    acepto.classList.remove("formulario__oculto");
 } );
 chk_radio_r.addEventListener("click",() => {
-    alert("rechazoo");
+    //alert("rechazoo");
+
+    acepto.classList.add("formulario__oculto");
+    acepto.classList.remove("formulario__oculto--visible");
 } );
