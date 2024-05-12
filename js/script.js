fed73c1fc591549e8500e4ba1729d218a37350ff
ceijoan <166540233+ceijoan@users.noreply.github.com>
2024-05-12 19:04:29 +0200

    Creacion de proyecto y primera pagina

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const chk_radio_a =document.querySelector('#acepto');
+const chk_radio_r =document.querySelector('#rechazo');
+chk_radio_a.addEventListener("click",() => {
+    alert("HOLAAAAA");
+} );
+chk_radio_r.addEventListener("click",() => {
+    alert("rechazoo");
+} );
